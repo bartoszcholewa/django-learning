4f00ec207b34b8ae27bd971e1c6e96e7663261eb
Bartosz Cholewa <bbcholewa@gmail.com>
2018-02-18 21:15:52 +0100

    Chapter 5 - Finished

--- src/images/urls.py
+++ src/images/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls import url
-from .views import image_create, image_detail, image_like
+from .views import image_create, image_detail, image_like, image_list
 
 urlpatterns = [
     url(r'^create/$', image_create, name='create'),
     url(r'detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$', image_detail, name='detail'),
     url(r'^like/$', image_like, name='like'),
+    url(r'^$', image_list, name='list'),
 ]
